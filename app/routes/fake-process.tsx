d2ffeaef50015278fe622f3061747fb7a7e6af5a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-03-05 13:06:03 -0300

    Remove `measureTime` usage
    I need the simplest version of this SSE thingy.

--- app/routes/fake-process.tsx
+++ app/routes/fake-process.tsx
@@ -1,30 +1,23 @@
-import type { LoaderFunctionArgs } from "@remix-run/node"
-
 import { useLoaderData } from "@remix-run/react"
 
 import { logger } from "~/only.server/logger"
 import { fakeProcess } from "~/only.server/fakeProcess"
-import { measureTime } from "~/only.server/lib/measureTime"
 
-export const loader = async (): LoaderFunctionArgs => {
-  const [result, timeTaken] = await measureTime(fakeProcess())
+export const loader = async () => {
+  const fakeResult = await fakeProcess()
 
-  logger.info("Some backend process", {
-    result,
-    timeTaken,
-  })
+  logger.info("Some backend process", { fakeResult })
 
-  return { result, timeTaken }
+  return { fakeResult }
 }
 
 export default function FakeProcess() {
-  const { result, timeTaken } = useLoaderData<typeof loader>()
+  const { fakeResult } = useLoaderData<typeof loader>()
 
   return (
     <div>
       <ul>
-        <li><strong>Result:</strong>{result}</li>
-        <li><strong>Time taken:</strong>{timeTaken}</li>
+        <li><strong>Result:</strong>&nbsp;{fakeResult}</li>
       </ul>
     </div>
   )
